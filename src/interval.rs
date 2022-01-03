c1a775109dc004c84ab1524637069b2684d34dd5
Teoh Han Hui <teohhanhui@gmail.com>
2022-01-03 16:34:35 +0800

    Add missing example for interval

--- src/interval.rs
+++ src/interval.rs
@@ -14,6 +14,42 @@ use crate::{Message, Source};
 /// A callbag listenable source that sends incremental numbers every x milliseconds.
 ///
 /// See <https://github.com/staltz/callbag-interval/blob/45d4fd8fd977bdf2babb27f67e740b0ff0b44e1e/index.js#L1-L10>
+///
+/// # Examples
+///
+/// ```
+/// use arc_swap::ArcSwap;
+/// use async_nursery::Nursery;
+/// use std::{sync::Arc, time::Duration};
+///
+/// use callbag::{for_each, interval};
+///
+/// let (nursery, nursery_out) = Nursery::new(async_executors::AsyncStd);
+///
+/// let vec = Arc::new(ArcSwap::from_pointee(vec![]));
+///
+/// let source = interval(Duration::from_millis(1_000), nursery.clone());
+///
+/// for_each({
+///     let vec = Arc::clone(&vec);
+///     move |x| {
+///         println!("{}", x);
+///         vec.rcu(move |vec| {
+///             let mut vec = (**vec).clone();
+///             vec.push(x);
+///             vec
+///         });
+///     }
+/// })(source);
+///
+/// drop(nursery);
+/// async_std::task::block_on(async_std::future::timeout(
+///     Duration::from_millis(4_500),
+///     nursery_out,
+/// ));
+///
+/// assert_eq!(vec.load()[..], [0, 1, 2, 3]);
+/// ```
 pub fn interval(
     period: Duration,
     nursery: impl Nurse<()> + Send + Sync + 'static,
